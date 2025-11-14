08f74b71cdeb3238bd7b8d2b467d91d02d870b94
Ramagiri Ranjith <naniranjith720@gmail.com>
2025-11-14 09:20:38 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { MapPin, Phone, Mail, Clock, Instagram, Facebook, Music } from "lucide-react";
 import logo from './../assets/logo.png';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
 
 const navigation = [
   { name: "Home", href: "/" },
